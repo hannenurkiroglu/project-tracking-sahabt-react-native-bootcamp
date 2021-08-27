2ff469233d58e184d7d0bfda33b01bf3f731dc5b
hannenurkiroglu <hannenur.yazbahar@bil.omu.edu.tr>
2021-08-27 09:43:21 +0300

    CREATE: Multi Language files.

---
+++ src/i18n/en.js
@@ -0,0 +1,6 @@
+export default {
+  username: 'User Name',
+  password: 'Password',
+  rememberMe: 'Remember Me',
+  login: 'Login',
+};
---
+++ src/i18n/index.js
@@ -0,0 +1,33 @@
+import I18n from 'i18n-js';
+import * as RnLocalize from 'react-native-localize';
+import {I18nManager} from 'react-native';
+import en from './en';
+import tr from './tr';
+
+// getLocales = Cihazın tercih edilen dil dosyasını getirir.
+const locales = RnLocalize.getLocales();
+
+console.log('locales', locales);
+
+// languageTag = Dilimizin dil kodunu getiriyor.
+I18n.locale = locales[0].languageTag;
+
+// isRTL = İlgili dilin sağdan sola mı yoksa soldan sağa mı yazıldığını belirtir.
+export const isRtl = locales[0].isRTL;
+
+// forceRtl = Arayüzdeki bileşenlerin soldan sağa yerine sağdan sola doğru dizilmesini zorunlu tutar.
+I18nManager.forceRTL = isRtl;
+
+// Belirtilen dile ait kayıt yoksa bir sonraki dilden devam etmesini sağlar.
+I18n.fallbacks = true;
+
+// İstenilen dil bulunmadığında yerine kullanılacak default değeri erir.
+I18n.locales.no = 'tr';
+
+// Uygulama içerisinde kullanılacak olan dillerin nesne olarak alınmasını sağlar.
+I18n.translations = {
+  tr,
+  en,
+};
+
+export default I18n;
