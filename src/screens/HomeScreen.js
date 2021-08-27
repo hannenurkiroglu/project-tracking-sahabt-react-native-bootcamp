4400fa932c3ba3cbb690e2b36153723a7801cdb2
hannenurkiroglu <hannenur.yazbahar@bil.omu.edu.tr>
2021-08-28 00:01:22 +0300

    ADD: userInfo to text

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {userLogout} from '../redux/system/actions';
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
+
+  const userInfo = useSelector(state => state.system.userInfo);
+  console.log('userınfo', userInfo);
 
   const logOut = () => {
     dispatch(userLogout());
@@ -12,7 +15,9 @@ export default function HomeScreen() {
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home Screen Hoş Geldin Adım Soyadım</Text>
+      <Text>
+        Home Screen Hoş Geldin {userInfo.name} {userInfo.surname}
+      </Text>
       <Button title="Çıkış Yap" onPress={() => logOut()} />
     </View>
   );
