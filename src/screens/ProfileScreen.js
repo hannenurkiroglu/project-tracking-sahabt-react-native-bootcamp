6932dc6ec85b64fc198e4ecf56048ccf51d3011c
hannenurkiroglu <hannenur.yazbahar@bil.omu.edu.tr>
2021-09-02 17:24:27 +0300

    ADD: user information to ProfileScreen.js

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,23 +1,129 @@
 import React from 'react';
-import {View, Text, Switch} from 'react-native';
-import {useSelector, useDispatch} from 'react-redux';
+import {
+  View,
+  Text,
+  Image,
+  Switch,
+  StyleSheet,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import {useDispatch} from 'react-redux';
 import {fonts, colors} from '../constants';
-import {setTheme} from '../redux/system/actions';
+import {setTheme, userLogout} from '../redux/system/actions';
 import CustomView from '../components/CustomView';
 import Header from '../components/Header';
+import I18n from '../i18n';
+import {GetIsDarkMode, GetUserInfo} from '../redux/system/selectors';
 
 export default function ProfileScreen() {
-  const isDarkMode = useSelector(state => state.system.isDarkMode);
+  const logOutText = I18n.t('logOut');
+
+  const isDarkMode = GetIsDarkMode();
+
+  const userInfo = GetUserInfo();
 
   const dispatch = useDispatch();
 
   const toggleTheme = val => {
     dispatch(setTheme(val));
   };
+
+  const logOut = () => {
+    dispatch(userLogout());
+  };
+
   return (
-    <CustomView style={{flex: 1}}>
+    <CustomView style={styles.container}>
       <Header title="Profile" />
-      <Switch onValueChange={val => toggleTheme(val)} value={isDarkMode} />
+
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        showsVerticalScrollIndicator={false}>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <View>
+            <View style={styles.infoBox}>
+              <Image
+                source={{uri: userInfo.profilePic}}
+                style={{width: 100, height: 100}}
+                resizeMethod="scale"
+                resizeMode="contain"
+              />
+              <View style={styles.cell}>
+                <Text style={styles.info}>Ünvan</Text>
+                <Text style={styles.info}>{userInfo.title}</Text>
+              </View>
+              <View style={styles.cell}>
+                <Text style={styles.info}>Şirket Adı</Text>
+                <Text style={styles.info}>{userInfo.company}</Text>
+              </View>
+              <View style={styles.cell}>
+                <Text style={styles.info}>{userInfo.displayName}</Text>
+              </View>
+              <View style={styles.cell}>
+                <Text style={styles.info}>{userInfo.mobile}</Text>
+              </View>
+              <View style={styles.cell}>
+                <Text style={styles.info}>{userInfo.managerDisplayName}</Text>
+              </View>
+              <View style={styles.cell}>
+                <Text style={styles.info}>{userInfo.unitName}</Text>
+              </View>
+              <View style={styles.cell}>
+                <Text style={styles.info}>{userInfo.profilePic}</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                <Switch
+                  onValueChange={val => toggleTheme(val)}
+                  value={isDarkMode}
+                />
+                <Text style={{color: colors.cFFFFFF, marginHorizontal: 10}}>
+                  Tema Seçimi
+                </Text>
+              </View>
+              <View>
+                <TouchableOpacity onPress={() => logOut()} style={styles.title}>
+                  <Text style={{fontSize: fonts.f14, color: colors.white[100]}}>
+                    {logOutText}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </CustomView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+  scrollView: {
+    paddingBottom: 20,
+    marginTop: Platform.OS === 'android' ? 15 : 0,
+  },
+  cell: {flex: 1},
+  topBackground: {},
+  title: {marginTop: 10, fontSize: fonts.f12, marginBottom: 5},
+  info: {
+    fontSize: fonts.f13,
+    color: colors.white[100],
+  },
+  infoBox: {
+    marginTop: -10,
+    marginHorizontal: 30,
+    padding: 20,
+    elevation: 3,
+  },
+  infoContainer: {
+    marginTop: 5,
+    paddingBottom: 10,
+    borderWidth: 0.5,
+    borderColor: colors.dark.primary[5],
+  },
+});
