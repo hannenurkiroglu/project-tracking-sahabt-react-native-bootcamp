ba8cd012f1e0fae3a5969cfa66f55fd0912bf90c
hannenurkiroglu <hannenur.yazbahar@bil.omu.edu.tr>
2021-09-03 17:41:52 +0300

    ADD: borderRadius to make profile photo round

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -158,7 +158,8 @@ const styles = StyleSheet.create({
   profileImage: {
     width: 100,
     height: 100,
-    borderRadius: 50,
+    // to make profile photo round = borderRadius: 50 (IOS)
+    borderRadius: 100,
   },
   cell: {flex: 1},
   topBackground: {},
