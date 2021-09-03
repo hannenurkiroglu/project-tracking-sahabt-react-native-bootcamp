033b8f3fee62a0826ae885269021cbd377747b1f
hannenurkiroglu <hannenur.yazbahar@bil.omu.edu.tr>
2021-09-03 16:17:53 +0300

    ADD: Dropdown menus to HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {fonts, colors} from '../constants';
 import CustomView from '../components/CustomView';
 import {userLogout} from '../redux/system/actions';
 import Button from '../components/Button';
 import Header from '../components/Header';
+import Input from '../components/Input';
+import Dropdown from '../components/Dropdown';
 import I18n from '../i18n';
 
 export default function HomeScreen() {
+  const [pageData, setPageData] = useState({
+    description: '',
+    projectId: null,
+    time: '',
+  });
+
   const logOutText = I18n.t('logOut');
 
   const isDarkMode = useSelector(state => state.system.isDarkMode);
@@ -22,22 +30,75 @@ export default function HomeScreen() {
   const userInfo = useSelector(state => state.system.userInfo);
   console.log('userInfo', userInfo);
 
-  const logOut = () => {
-    dispatch(userLogout());
+  const handleLanguageChange = lang => {};
+
+  const onChangeText = (key, text) => {
+    setPageData(page => ({...page, [key]: text}));
   };
+
+  // FIX: Does it work on android? => onDonePress ?
+  const onDonePress = () => {};
+
+  const saveProjectTimeline = () => {};
 
   return (
     <CustomView style={{flex: 1}}>
       <Header title="Home" />
-      <Text
-        style={{
-          fontWeight: 'bold',
-          fontSize: fonts.f15,
-          color: isDarkMode ? colors.dark.text[100] : colors.light.white[100],
-        }}>
-        Home Screen Hoş Geldin {userInfo.name} {userInfo.surname}
-      </Text>
-      <Button onPress={() => logOut()} text={logOutText} />
+      <View style={{flex: 1, marginHorizontal: 15}}>
+        <View style={styles.inputContainer}>
+          <Dropdown
+            items={[
+              {label: 'Coffy', value: 1},
+              {label: 'Plugger', value: 2},
+              {label: 'Saha BT', value: 3},
+            ]}
+            value={language}
+            placeholder="Proje Seçiniz"
+            onValueChange={val => handleLanguageChange(val)}
+            onDonePress={() => onDonePress()}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Dropdown
+            items={[
+              {label: '1 Saat', value: 60},
+              {label: '2 Saat', value: 120},
+              {label: '3 Saat', value: 180},
+              {label: '4 Saat', value: 240},
+              {label: '5 Saat', value: 300},
+              {label: '6 Saat', value: 360},
+              {label: '7 Saat', value: 420},
+              {label: '8 Saat', value: 480},
+            ]}
+            value={language}
+            placeholder="Süre Seçiniz"
+            onValueChange={val => handleLanguageChange(val)}
+            onDonePress={() => onDonePress()}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Input
+            placeHolder="Proje açıklaması ekleyiniz"
+            // style={styles.inputContainer}
+            onChangeText={val => onChangeText('description', val)}
+            value={pageData.description}
+            multiline
+            color={colors.cFFFFFF}
+          />
+        </View>
+        <View>
+          <Button
+            onPress={() => saveProjectTimeline()}
+            text={'Çalışmamı Kaydet'}
+          />
+        </View>
+      </View>
     </CustomView>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 15,
+  },
+});
