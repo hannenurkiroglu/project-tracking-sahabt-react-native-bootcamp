57164bb76624ead70d9bbeefb2c3debc05024bbd
hannenurkiroglu <hannenur.yazbahar@bil.omu.edu.tr>
2021-09-06 03:40:58 +0300

    ADD: MapScreen to TabNavigation.js

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -3,6 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import TaskScreen from '../screens/TaskScreen';
+import MapScreen from '../screens/MapScreen';
 import TabBarIcon from '../components/TabBarIcon';
 import I18n from '../i18n';
 
@@ -14,6 +15,8 @@ export default function TabNavigator() {
   const profileTitle = I18n.t('profile');
 
   const TaskTitle = I18n.t('task');
+
+  const MapTitle = I18n.t('map');
 
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
@@ -31,6 +34,14 @@ export default function TabNavigator() {
         options={{
           tabBarIcon: () => <TabBarIcon name="person-outline" />,
           title: profileTitle,
+        }}
+      />
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          tabBarIcon: () => <TabBarIcon name="location-outline" />,
+          title: MapTitle,
         }}
       />
       <Tab.Screen
