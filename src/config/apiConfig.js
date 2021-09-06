6e2ff7fc4ff038c7e9b2631b9aabe54bcaf2de3a
hannenurkiroglu <hannenur.yazbahar@bil.omu.edu.tr>
2021-09-06 03:40:30 +0300

    ADD: randomuserapi to apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -6,7 +6,8 @@ const prefixes = {
 
 const apiConfig = {
   prefixes,
-  baseUrl: 'https://project-tracker-saha-bt.herokuapp.com',
+  // baseUrl: 'https://project-tracker-saha-bt.herokuapp.com',
+  baseUrl: 'https://randomuser.me/api/',
 };
 
 export default apiConfig;
