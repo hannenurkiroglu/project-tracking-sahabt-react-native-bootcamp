5d587cbc868d184abad1bc9257163db4bc0f0a50
hannenurkiroglu <hannenur.yazbahar@bil.omu.edu.tr>
2021-09-10 13:22:34 +0300

    UPDATE: apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -7,7 +7,7 @@ const prefixes = {
 const apiConfig = {
   prefixes,
   // baseUrl: 'https://project-tracker-saha-bt.herokuapp.com',
-  baseUrl: 'https://randomuser.me/api/',
+  baseUrl: 'https://randomuser.me/api',
 };
 
 export default apiConfig;
